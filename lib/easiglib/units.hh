c741d175efcdf713969014386b365b14fe87999e
Matthias Puech <matthias.puech@gmail.com>
2018-08-19 17:55:45 +0200

    Units: use new exp2() function to calculate pitch

--- lib/easiglib/units.hh
+++ lib/easiglib/units.hh
@@ -1,28 +1,39 @@
 #include "numtypes.hh"
 #include "data.hh"
 
-struct Pitch : Float {
+struct Pitch : private Float {
+  using T = Pitch;
+
+  explicit constexpr Pitch(Float x) : Float(x) {};
+  constexpr Float const repr() const { return *this; }
+
+  constexpr T const operator+(const T y) const { return T(repr() + y.repr()); }
+  constexpr T const operator-(const T y) const { return T(repr() - y.repr()); }
+  constexpr T const operator-() const { return T(-repr()); }
+  constexpr T const operator*(const T y) const { return T((repr() * y.repr())); }
+  constexpr T const operator/(const T y) const { return T((repr()) / y.repr()); }
+
+  constexpr bool const operator<(const T y) const { return repr() < y.repr(); }
+  constexpr bool const operator>(const T y) const { return repr() > y.repr(); }
+  constexpr bool const operator<=(const T y) const { return repr() <= y.repr(); }
+  constexpr bool const operator>=(const T y) const { return repr() >= y.repr(); }
+  constexpr bool const operator==(const T y) const { return repr() == y.repr(); }
+  constexpr bool const operator!=(const T y) const { return repr() != y.repr(); }
 };
+
+constexpr Pitch operator "" _st(long double x) { return Pitch(f(x)); }
 
 struct Freq : private Float {
 
-  explicit constexpr Freq(Float x) : Float(x / (Float)kSampleRate) { };
+  static constexpr f semitones_to_ratio(f p) {
+    return Math::fast_exp2(p/12_f);
+  }
+
+  explicit constexpr Freq(Float x) : Float(x / f(kSampleRate)) {};
+  explicit constexpr Freq(Pitch p) : Freq(semitones_to_ratio(p.repr() - 69._f) * 440_f) { };
 
   constexpr Float const repr() const { return *this; }
 
-  static Float semitones_to_ratio(Float semitones) { // -127..128
-    semitones += 128_f;
-    index integral = index(semitones); // 0..255
-    Float fractional = semitones.fractional();
-    index low_index = index((fractional * Float(Data::pitch_ratios_low.size().repr())));
-    return
-      Data::pitch_ratios_high[integral] *
-      Data::pitch_ratios_low[low_index];
-  }
-
-  static Float of_midi(Float midi_pitch) {
-    return semitones_to_ratio(midi_pitch - 69_f) * 440_f;
-  }
 };
 
 constexpr Freq operator "" _Hz(long double x) { return Freq(f(x)); }
