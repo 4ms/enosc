461e78616895855c656734f5834f55f0e12c7caa
Matthias Puech <matthias.puech@gmail.com>
2018-10-24 18:02:41 +0200

    Parameters: restore previous code
    Will be needed later

--- src/parameters.hh
+++ src/parameters.hh
@@ -1,2 +1,11 @@
+#include <cstdint>
 
+#pragma once
+
+constexpr struct ShortFrame {
+  int16_t l = 0;
+  int16_t r = 0;
+} zero;
+
+constexpr int kBlockSize = 32;
 constexpr int kSampleRate = 96000;
