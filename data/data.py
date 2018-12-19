87d83b14fa5d2cd00b62878c95d2878c703f7533
Matthias Puech <matthias.puech@gmail.com>
2018-12-19 14:34:09 +0100

    Data: optimize LUT sizes

--- data/data.py
+++ data/data.py
@@ -4,19 +4,9 @@ import numpy as np
 
 data = {}
 
-# Pitch to frequency conversion
-
-size = 256
-ratios_low = 2.0 ** (np.arange(-size/2.0,size/2.0)/12.0)
-data['pitch_ratios_high'] = ratios_low
-
-size = 256
-ratios_high = 2.0 ** (np.linspace(0.,1./12.,num=size))
-data['pitch_ratios_low'] = ratios_high
-
 # sine
 
-size = 1024
+size = 256
 spc = np.arange(0., size+1)/size
 sine = np.sin(spc*2*np.pi)
 data['sine'] = sine
@@ -25,7 +15,7 @@ data['short_sine'] = (sine*32767.0).astype(np.int16)
 
 # base-2 exponential
 
-size = 1024
+size = 256
 spc = np.arange(0., size) / size
 exp2 = 2 ** spc
 
