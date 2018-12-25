42a39387564c007931ab7c2b3220bfa184243a47
Matthias Puech <matthias.puech@gmail.com>
2018-12-25 15:15:38 +0100

    Ui: (WIP) combine pot and CV

--- src/ui.hh
+++ src/ui.hh
@@ -12,67 +12,82 @@ class Control {
 
   enum Law { LINEAR, QUADRATIC, CUBIC, QUARTIC };
 
-  template<Law LAW, int LP>  // Lp = 0..16
+  template<Law LAW>  // Lp = 0..16
   class PotConditioner {
-    QuadraticOnePoleLp<LP> lp_;
   public:
-    s1_15 Process(Adc::Channel ch) {
-      u0_16 in = ch.get();
+    u0_16 Process(Adc::Channel ch) {
+      u0_16 x = ch.get();
       switch(LAW) {
       case LINEAR: break;
-      case QUADRATIC: in = u0_16::narrow(in * in); break;
-      case CUBIC: in = u0_16::narrow(u0_16::narrow(in * in) * in); break;
+      case QUADRATIC: x = u0_16::narrow(x * x); break;
+      case CUBIC: x = u0_16::narrow(u0_16::narrow(x * x) * x); break;
       case QUARTIC:
-        in = u0_16::narrow(in * in);
-        in = u0_16::narrow(in * in);
+        x = u0_16::narrow(x * x);
+        x = u0_16::narrow(x * x);
         break;
       }
-      s1_15 x = in.to_signed();
-      x = lp_.Process(x);
       return x;
     }
   };
 
   struct CVConditioner {
     s1_15 Process(Adc::Channel ch) {
+      // TODO calibration
       s1_15 x = ch.get().to_signed_scale();
       return x;                 // -1..1
     }
   };
 
+  struct None { s1_15 Process(Adc::Channel) { return 0._s1_15; } };
+
+  template<class PotConditioner, class CVConditioner, int LP>
+  struct PotCVCombiner {
+    PotConditioner pot_;
+    CVConditioner cv_;
+    QuadraticOnePoleLp<LP> lp_;
+    f Process(Adc::Channel pot, Adc::Channel cv) {
+      s1_15 x = pot_.Process(pot).to_signed();
+      x = x.sub_sat(cv_.Process(cv));
+      if (x<0._s1_15) x=0._s1_15; // TODO use saturating add
+      return lp_.Process(x.to_float_inclusive());
+    }
+    f Process(Adc::Channel pot) {
+      f x = pot_.Process(pot).to_float_inclusive();
+      return lp_.Process(x);
+    }
+  };
+
   class AudioCVConditioner {
+    QuadraticOnePoleLp<2> lp_;
     CicDecimator<1, kBlockSize> cic_;
   public:
     f Process(Block<s1_15> in) {
       s1_15 x = in[0];
       cic_.Process(in.begin(), &x, 1);
-      return x.to_float_inclusive();
+      return lp_.Process(x.to_float_inclusive());
     }
   };
 
   Adc adc_;
 
-  PotConditioner<LINEAR, kPotFiltering> warp_pot;
-  PotConditioner<LINEAR, kPotFiltering> detune_pot;
-  PotConditioner<LINEAR, kPotFiltering> mod_pot;
-  PotConditioner<LINEAR, kPotFiltering> root_pot;
-  PotConditioner<LINEAR, kPotFiltering> grid_pot;
-  PotConditioner<LINEAR, kPotFiltering> pitch_pot;
-  PotConditioner<LINEAR, kPotFiltering> spread_pot;
-  PotConditioner<LINEAR, kPotFiltering> tilt_pot;
-  PotConditioner<LINEAR, kPotFiltering> twist_pot;
 
-  CVConditioner warp_cv;
-  CVConditioner spread_cv;
-  CVConditioner twist_cv;
-  CVConditioner tilt_cv;
-  CVConditioner grid_cv;
-  CVConditioner mod_cv;
+  PotCVCombiner<PotConditioner<LINEAR>, None, kPotFiltering> detune_;
+  PotCVCombiner<PotConditioner<LINEAR>, CVConditioner, kPotFiltering> warp_;
+  PotCVCombiner<PotConditioner<LINEAR>, CVConditioner, kPotFiltering> tilt_;
+  PotCVCombiner<PotConditioner<LINEAR>, CVConditioner, kPotFiltering> twist_;
+  PotCVCombiner<PotConditioner<LINEAR>, CVConditioner, kPotFiltering> grid_;
+  PotCVCombiner<PotConditioner<LINEAR>, CVConditioner, kPotFiltering> mod_;
+  PotCVCombiner<PotConditioner<LINEAR>, CVConditioner, kPotFiltering> spread_;
 
+  // TODO encapsuler
+  PotConditioner<LINEAR> root_pot;
+  PotConditioner<LINEAR> pitch_pot;
   AudioCVConditioner pitch_cv;
   AudioCVConditioner root_cv;
-
-  QuadraticOnePoleLp<4> tilt_lp;
+  QuadraticOnePoleLp<2> root_pot_lp_;
+  QuadraticOnePoleLp<2> pitch_pot_lp_;
+  QuadraticOnePoleLp<2> root_cv_lp_;
+  QuadraticOnePoleLp<2> pitch_cv_lp_;
 
 public:
   void Process(Block<Frame> codec_in, Parameters &params) {
@@ -88,36 +103,28 @@ public:
       *i2++ = in.r;
     }
 
-    f p_cv = pitch_cv.Process(Block<s1_15> {in1, size});
-    f r_cv = root_cv.Process(Block<s1_15> {in2, size});
-
-    p_cv = 0_f;                 // TODO
-    r_cv = 0_f;                 // TODO
+    // Root & Pitch
+    f p_cv = pitch_cv.Process(Block<s1_15> {in1, size}); // -1..1
+    f r_cv = root_cv.Process(Block<s1_15> {in2, size});  // -1..1
 
     // Process potentiometer & CV
 
-    s1_15 de = detune_pot.Process(adc_.detune_pot);
-    f detune = Math::crop_down(kPotDeadZone, de.to_float_inclusive());
+    f detune = detune_.Process(adc_.detune_pot);
+    detune = Math::crop_down(kPotDeadZone, detune);
     detune = (detune * detune) * (detune * detune);
     params.detune = detune;
 
-    s1_15 ti = tilt_pot.Process(adc_.tilt_pot);
-    ti = ti.sub_sat(tilt_cv.Process(adc_.tilt_cv));
-    ti = tilt_lp.Process(ti);
-    f tilt = Math::crop(kPotDeadZone, ti.to_float_inclusive());
+    f tilt = tilt_.Process(adc_.tilt_pot, adc_.tilt_cv);
+    tilt = Math::crop(kPotDeadZone, tilt);
     tilt = tilt * 2_f - 1_f;
     tilt *= tilt.abs();
     tilt *= 8_f;
     tilt = Math::fast_exp2(tilt);
     params.tilt = tilt;
 
-    s1_15 wa = warp_pot.Process(adc_.warp_pot);
-    f warp = wa.to_float_inclusive();
-    params.warp.value = warp;
+    params.warp.value = warp_.Process(adc_.warp_pot);
 
-    s1_15 tw = twist_pot.Process(adc_.twist_pot);
-    f twist = tw.to_float_inclusive();
-
+    f twist = twist_.Process(adc_.twist_pot, adc_.twist_cv);
     if (params.twist.mode == FEEDBACK) {
       twist *= twist;
     } else if (params.twist.mode == PULSAR) {
@@ -126,40 +133,26 @@ public:
     } else if (params.twist.mode == DECIMATE) {
       twist *= twist * 0.5_f;
     }
-
     params.twist.value = twist;
 
-    s1_15 root = root_pot.Process(adc_.root_pot);
-    params.root = root.to_float_inclusive() * 12_f * 10_f;
-    params.root += r_cv * 12_f * 4_f;
+    // TODO encapsuer ds classe
+    u0_16 r = root_pot.Process(adc_.root_pot);
+    f root = root_pot_lp_.Process(r.to_float_inclusive());
+    root *= 12_f * 10_f; // 0..120
+    root += root_cv_lp_.Process(r_cv) * 12_f * 4_f;
+    params.root = root;
 
-    s1_15 pitch = pitch_pot.Process(adc_.pitch_pot);
-    params.pitch = pitch.to_float_inclusive() * 12_f * 6_f - 24_f;
-    params.pitch += p_cv * 12_f * 4_f;
+    u0_16 p = pitch_pot.Process(adc_.pitch_pot);
+    f pitch = pitch_pot_lp_.Process(p.to_float_inclusive());
+    pitch = pitch * 12_f * 6_f - 24_f;
+    pitch += pitch_cv_lp_.Process(p_cv) * 12_f * 4_f;
+    params.pitch = pitch;
     
-    s1_15 s = spread_pot.Process(adc_.spread_pot);
-    f spread = Math::crop_down(kPotDeadZone, s.to_float_inclusive());
+    f spread = spread_.Process(adc_.spread_pot);
     spread *= spread;
     params.spread = spread * 12_f;
 
-    // warp_pot = adc_.get(Adc::WARP_POT);
-    // detune_pot = adc_.get(Adc::DETUNE_POT);
-    // mod_pot = adc_.get(Adc::MOD_POT);
-    // root_pot = adc_.get(Adc::ROOT_POT);
-    // grid_pot = adc_.get(Adc::GRID_POT);
-    // pitch_pot = adc_.get(Adc::PITCH_POT);
-    // spread_pot = adc_.get(Adc::SPREAD_POT);
-    // tilt_pot = adc_.get(Adc::TILT_POT);
-    // twist_pot = adc_.get(Adc::TWIST_POT);
-
-    // spread1_cv = adc_.get(Adc::SPREAD_CV_1);
-    // warp_cv = adc_.get(Adc::WARP_CV);
-    // spread2_cv = adc_.get(Adc::SPREAD_CV_2);
-    // twist_cv = adc_.get(Adc::TWIST_CV);
-    // tilt_cv = adc_.get(Adc::TILT_CV);
-    // grid_cv = adc_.get(Adc::GRID_CV);
-    // mod_cv = adc_.get(Adc::MOD_CV);
-
+    // 
     adc_.Start();
   }
 };
