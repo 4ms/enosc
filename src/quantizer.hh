19a7b8b86cbdcabe1df452b41985d6040e4cf661
Matthias Puech <matthias.puech@gmail.com>
2018-12-27 13:43:43 +0100

    Quantizer: set the repeat interval to the maximum interval

--- src/quantizer.hh
+++ src/quantizer.hh
@@ -1,19 +1,28 @@
 #include "buffer.hh"
 
 constexpr const int kGridNr = 10;
-constexpr const f kRepeatSt = 14_f;
+constexpr const int kMaxGridSize = 16;
 
 class Grid {
-  f grid[5] = {0_f, 3_f, 7_f, 10_f, f(kRepeatSt)};
-  int size = 5;
+  f grid[kMaxGridSize];
+  int size = 0;
 
 public:
+  Grid() {
+    grid[0] = 0_f;
+    grid[1] = 3_f;
+    grid[2] = 7_f;
+    grid[3] = 12_f;
+    size = 4;
+  }
+
   void Process(f pitch, f &p1, f &p2, f &phase) {
-
-    int oct = (pitch / kRepeatSt).floor();
-    f octaves = f(oct) * kRepeatSt;
+    f max = grid[size-1];
+    // quotient by the max
+    int oct = (pitch / max).floor();
+    f octaves = f(oct) * max;
     f semitones = pitch - octaves;
-
+    // invariant: 0 < semitones < max
     int index = binary_search(semitones, grid, size);
     p1 = grid[index];
     p2 = grid[index+1];
