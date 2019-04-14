c14c1664abc1336bd4a72d0bc759c32b318197cd
Matthias Puech <matthias.puech@gmail.com>
2019-04-14 23:25:23 +0200

    Signal: more crossfade variants

--- lib/easiglib/signal.hh
+++ lib/easiglib/signal.hh
@@ -11,8 +11,16 @@ struct Signal {
     return crossfade(x, y, phase.to_float());
   }
 
+  static constexpr f crossfade(f x, f y, u0_32 phase) {
+    return crossfade(x, y, phase.to_float());
+  }
+
   static constexpr s1_15 crossfade(s1_15 x, s1_15 y, u0_32 phase) {
     return x + s1_15::narrow((y - x) * u0_16::narrow(phase).to_signed());
+  }
+
+  static constexpr s1_15 crossfade(s1_15 x, s1_15 y, u0_16 phase) {
+    return x + s1_15::narrow((y - x) * phase.to_signed());
   }
 
   // (p..1 -> 0..1)
