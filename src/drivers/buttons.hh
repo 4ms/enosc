5aee3c15c17412b82e11aa77fda6dfba2c05c2c0
Matthias Puech <matthias.puech@gmail.com>
2019-04-18 00:32:59 +0200

    Buttons: debounce on startup

--- src/drivers/buttons.hh
+++ src/drivers/buttons.hh
@@ -29,6 +29,7 @@ struct Buttons : Nocopy {
       gpio.Mode = GPIO_MODE_INPUT;
       gpio.Pull = GPIO_PULLUP;
       HAL_GPIO_Init(GPIOC, &gpio);
+      for (int i=0; i<16; i++) Debounce();
     }
     bool get() { return ReadPin(GPIOC, GPIO_PIN_9); };
   } learn_;
@@ -41,6 +42,7 @@ struct Buttons : Nocopy {
       gpio.Mode = GPIO_MODE_INPUT;
       gpio.Pull = GPIO_PULLUP;
       HAL_GPIO_Init(GPIOA, &gpio);
+      for (int i=0; i<16; i++) Debounce();
     }
     bool get() { return ReadPin(GPIOA, GPIO_PIN_11); };
   } freeze_;
