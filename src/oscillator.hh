0808ad68746ae7ce0d5cf29073a3a964cdf3aae1
Matthias Puech <matthias.puech@gmail.com>
2019-04-28 01:50:16 +0200

    Oscillator, Polyptic: tentative new architecture
    A common waveform is precomputed for all voices

--- src/oscillator.hh
+++ src/oscillator.hh
@@ -4,6 +4,8 @@
 #include "distortion.hh"
 #include "dynamic_data.hh"
 
+constexpr int kWaveformBufferSize = 512;
+
 class Phasor {
   u0_32 phase_ = u0_32::of_repr(Random::Word());
 public:
@@ -33,34 +35,41 @@ public:
 
 class Oscillator {
   Phasor phasor_;
-  SineShaper sine_shaper_;
   IFloat fader_ {0_f};
+
+  static Buffer<f, kWaveformBufferSize+1> waveform;
 
 public:
   template<TwistMode twist_mode, WarpMode warp_mode>
-  static f Process(Phasor& ph, SineShaper& sh, u0_32 freq, u0_16 mod, f twist_amount, f warp_amount) {
-    u0_32 phase = ph.Process(freq);
-    phase += u0_32(mod);
-    phase = Distortion::twist<twist_mode>(phase, twist_amount);
+  static void ComputeWaveform(f twist_amount, f warp_amount) {
+    Phasor ph;
+    SineShaper sh;
+    constexpr u0_32 freq = u0_32::max_val / kWaveformBufferSize;
 
-    s1_15 sine = twist_mode == FEEDBACK ?
-      sh.Process(phase, u0_16(twist_amount)) :
-      sh.Process(phase);
+    for (f& sample : waveform) {
+      u0_32 phase = ph.Process(freq);
+      phase = Distortion::twist<twist_mode>(phase, twist_amount);
 
-    return Distortion::warp<warp_mode>(sine, warp_amount);
+      s1_15 sine = twist_mode == FEEDBACK ?
+        sh.Process(phase, u0_16(twist_amount)) :
+        sh.Process(phase);
+
+      sample = Distortion::warp<warp_mode>(sine, warp_amount);
+    }
   }
 
-  template<TwistMode twist_mode, WarpMode warp_mode, int block_size>
-  void Process(u0_32 const freq, f const twist, f const warp,
-               f const fade, f const amplitude, f const modulation,
-               Block<u0_16, block_size> mod_in, Block<u0_16, block_size> mod_out, Block<f, block_size> sum_output) {
+  template<int block_size>
+  void Process(u0_32 const freq, f const fade, f const amplitude, f const modulation,
+               Block<u0_16, block_size> mod_in, Block<u0_16, block_size> mod_out,
+               Block<f, block_size> sum_output) {
     Phasor ph = phasor_;
-    SineShaper sh = sine_shaper_;
     IFloat fd = fader_;
     fd.set(fade, block_size);
 
     for (auto [sum, m_in, m_out] : zip(sum_output, mod_in, mod_out)) {
-      f sample = Process<twist_mode, warp_mode>(ph, sh, freq, m_in, twist, warp);
+      u0_32 phase = ph.Process(freq);
+      phase += u0_32(m_in);
+      f sample = waveform.interpolate(phase);
       sample *= fd.next();
       // TODO comprendre +1
       m_out += u0_16((sample + 1_f) * modulation);
@@ -68,10 +77,11 @@ public:
     }
 
     phasor_ = ph;
-    sine_shaper_ = sh;
     fader_ = fd;
   }
 };
+
+inline Buffer<f, kWaveformBufferSize+1> Oscillator::waveform;
 
 struct FrequencyPair { f freq1, freq2, crossfade; };
 
@@ -87,9 +97,9 @@ class OscillatorPair : Nocopy {
 public:
   void set_freeze(bool b) { frozen = b; }
 
-  template<TwistMode twist_mode, WarpMode warp_mode, int block_size>
+  template<int block_size>
   void Process(FrequencyPair freq, f crossfade_factor,
-               f const twist, f const warp, f const amplitude, f const modulation,
+               f const amplitude, f const modulation,
                Block<u0_16, block_size> mod_in, Block<u0_16, block_size> mod_out,
                Block<f, block_size> sum_output) {
     if (frozen) freq = previous_freq;
@@ -111,11 +121,11 @@ public:
 
     // mod_out is accumulated in the two calls, so we need to zero it here
     mod_out.fill(0._u0_16);
-    osc_[0].Process<twist_mode, warp_mode>(u0_32(freq.freq1), twist, warp,
-                                           fade1, amplitude, modulation,
-                                           mod_in, mod_out, sum_output);
-    osc_[1].Process<twist_mode, warp_mode>(u0_32(freq.freq2), twist, warp,
-                                           fade2, amplitude, modulation,
-                                           mod_in, mod_out, sum_output);
+    osc_[0].Process(u0_32(freq.freq1),
+                    fade1, amplitude, modulation,
+                    mod_in, mod_out, sum_output);
+    osc_[1].Process(u0_32(freq.freq2),
+                    fade2, amplitude, modulation,
+                    mod_in, mod_out, sum_output);
   }
 };
