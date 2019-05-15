3c0d2818002bccf421f05b44b39b978466e5bbc9
Matthias Puech <matthias.puech@gmail.com>
2019-05-15 09:38:52 +0200

    Bitfield: add bitwise ops; Distortion: convert to Bitfield

--- lib/easiglib/bitfield.hh
+++ lib/easiglib/bitfield.hh
@@ -1,15 +1,22 @@
+#pragma once
+
 #include "numtypes.hh"
 
 template<int WIDTH>
 struct Bitfield {
   using Base = typename Basetype<WIDTH, UNSIGNED>::T;
 
-  explicit Bitfield(Base val) : val_(val) {}
+  explicit constexpr Bitfield(Base const val) : val_(val) {}
+
+  template<sign SIGN, int INT, int FRAC>
+  Bitfield(Fixed<SIGN, INT, FRAC> x) : val_(x.repr()) {}
+
   Base repr() { return val_; }
 
   bool is_set(int i) {
     return (val_ & (1 << i)) != 0;
   }
+
   Bitfield set(int i) {
     return Bitfield(val_ | (1U << i));
   }
@@ -26,7 +33,12 @@ struct Bitfield {
     }
     return count; 
   }
-  
+
+  Bitfield operator&(Bitfield that) { return Bitfield(val_ & that.val_); }
+  Bitfield operator|(Bitfield that) { return Bitfield(val_ | that.val_); }
+  Bitfield operator^(Bitfield that) { return Bitfield(val_ ^ that.val_); }
+
+  void operator^=(Bitfield that) { *this = *this ^ that; }
 
 private:
   Base val_ = 0;
