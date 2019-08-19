9c8ba3a6c1b0857b1cae372be7661b91baf7da02
Matthias Puech <matthias.puech@gmail.com>
2019-08-19 16:59:43 +0200

    DynamicData: compute fold table

--- src/dynamic_data.cc
+++ src/dynamic_data.cc
@@ -3,6 +3,7 @@
 
 Buffer<std::pair<s1_15, s1_15>, sine_size> DynamicData::sine;
 Buffer<Buffer<f, cheby_size>, cheby_tables> DynamicData::cheby;
+Buffer<std::pair<f, f>, fold_size> DynamicData::fold;
 
 DynamicData::DynamicData() {
 
@@ -28,4 +29,18 @@ DynamicData::DynamicData() {
   for (int n=2; n<cheby_tables; n++)
     for (int i=0; i<cheby_size; i++)
       cheby[n][i] = 2_f * cheby[0][i] * cheby[n-1][i] - cheby[n-2][i];
+
+  // fold
+  { f folds = 6_f;
+    f previous = 0_f;
+    for (int i=0; i<fold_size; ++i) {
+      f x = folds * f(i+1) / f(fold_size);
+      f g = 1_f / (1_f + x.abs());
+      f p = 16_f / (2_f * Math::pi) * x * g;
+      while(p > 1_f) p--;
+      x = g * (x + Math::fast_sine(p));
+      fold[i] = std::pair(previous, x - previous);
+      previous = x;
+    }
+  }
 }
--- src/dynamic_data.hh
+++ src/dynamic_data.hh
@@ -5,9 +5,11 @@
 static constexpr int sine_size = 512 + 1;
 static constexpr int cheby_tables = 16;
 static constexpr int cheby_size = 512 + 1;
+static constexpr int fold_size = 1024 + 1;
 
 struct DynamicData {
   DynamicData();
   static Buffer<std::pair<s1_15, s1_15>, sine_size> sine;
   static Buffer<Buffer<f, cheby_size>, cheby_tables> cheby;
+  static Buffer<std::pair<f, f>, fold_size> fold;
 };
