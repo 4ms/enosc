487e666ad97f1497de665f1a24ee7ce38b8b0511
Matthias Puech <matthias.puech@gmail.com>
2019-09-06 12:56:59 +0200

    PersistentStorage: first draft of class (WIP)

---
+++ lib/easiglib/persistent_storage.hh
@@ -0,0 +1,14 @@
+
+template <class T>
+class PersistentStorage : public T {
+  static constexpr int size = sizeof(T);
+public:
+  PersistentStorage(T default_data) {
+    *this = default_data;
+    // load into data_, failing back to default_data if not found
+  }
+
+  void Save() {
+    // save data_
+  }
+};
