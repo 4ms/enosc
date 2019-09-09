1b9ea7f0f8b3b48576beae98a28a4e3ff8996d03
Matthias Puech <matthias.puech@gmail.com>
2019-09-09 11:28:38 +0200

    Persistent: wear levelling draft class (WIP)
    Does not compile yet

--- lib/easiglib/persistent_storage.hh
+++ lib/easiglib/persistent_storage.hh
@@ -1,5 +1,23 @@
 #pragma once
 
+template<class Storage>
+class WearLevel {
+  int cell_ = Storage::cell_nr_;
+public:
+  using data_t = typename Storage::data_t;
+  static bool Read(data_t *data) {
+    while(--cell_) {
+      Storage::Read(data, cell);
+      if (data.validate()) return true;
+    }
+    return false;
+  }
+
+  static bool Write(data_t *data) {
+    return Storage::Write(data, cell++);
+  }
+};
+
 template <class Storage>
 class Persistent {
   using data_t = typename Storage::data_t;
@@ -7,10 +25,10 @@ class Persistent {
 public:
   Persistent(data_t *data, data_t const &default_data) : data_(data) {
     // load to data_, falling back to default_data if not found
-    bool success = Storage::Read(data_);
-    success &= data->validate();
-    if (!success) {
+    if (!Storage::Read(data_) ||
+        !data.validate()) {
       *data = default_data;
+      Save();
     }
   }
 
