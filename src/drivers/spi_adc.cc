5f93aeb856d20e59be306abe9d7d97b7969709c0
Dan Green <danngreen1@gmail.com>
2019-10-08 16:14:27 -0700

    debug: commented out

--- src/drivers/spi_adc.cc
+++ src/drivers/spi_adc.cc
@@ -1,6 +1,6 @@
 #include "spi_adc.hh"
 #include "debug.hh"
-extern Debug debug;
+// extern Debug debug;
 
 SpiAdc *SpiAdc::spiadc_instance_;
 uint32_t SpiAdc::os_idx[NUM_SPI_ADC_CHANNELS]={0};
@@ -12,7 +12,7 @@ extern "C" void MAX11666_SPI_IRQHANDLER(void) {
 
   if ((itflag & SPI_FLAG_RXNE) && (itsource & SPI_IT_RXNE))
   { 
-    debug.set(SpiAdc::cur_chan, true);
+    // debug.set(SpiAdc::cur_chan, true);
     uint16_t adc_val = SpiAdc::spiadc_instance_->spih.Instance->DR;
     adc_val >>= 2;
     if (SpiAdc::os_idx[SpiAdc::cur_chan] < kOversamplingAmount)
@@ -22,7 +22,7 @@ extern "C" void MAX11666_SPI_IRQHANDLER(void) {
       SpiAdc::os_idx[SpiAdc::cur_chan]--;
       SpiAdc::spiadc_instance_->spih.Instance->DR = SpiAdc::cur_chan ? MAX11666_CONTINUE_READING_CH2 : MAX11666_CONTINUE_READING_CH1;
    }
-   debug.set(SpiAdc::cur_chan, false);
+   // debug.set(SpiAdc::cur_chan, false);
 
   }
 }
