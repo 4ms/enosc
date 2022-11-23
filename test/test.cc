a6866390d8e21725acc3ecf3c157b5e2110afcc4
Matthias Puech <matthias.puech@gmail.com>
2022-11-23 08:43:33 +0100

    Test: update to compile
    It writes 10 seconds of audio with increasing root

--- test/test.cc
+++ test/test.cc
@@ -7,23 +7,56 @@
 
 constexpr int kDuration = 10;    // seconds
 
-struct Main {
-  WavWriter<Frame, 2> wav_{"test.wav", kDuration * kSampleRate};
+struct Main : Math, DynamicData {
+  WavWriter<std::pair<s1_15, s1_15>> wav_{"test.wav", kDuration * kSampleRate, 2, kSampleRate};
   Main() {
     int size = kDuration * kSampleRate;
 
-    Parameters params;
-    PolypticOscillator osc {[](bool){}, [](bool){}};
+    Parameters params = {
+      .balance = 1_f,
+      .root = 30_f,
+      .pitch = 30_f,
+      .spread = 0_f,
+      .detune = 0_f,
+      .modulation = {.mode = TWO, .value = 0_f},
+      .scale = {.mode = TWELVE, .value = 0},
+      .twist = {.mode = FEEDBACK, .value = 0_f},
+      .warp = {.mode = CHEBY, .value = 0_f},
+      .alt = {
+              .numOsc = 1,
+              .stereo_mode = ALTERNATE,
+              .freeze_mode = LOW_HIGH,
+              .crossfade_factor = 0.5_f,
+      },
+      .new_note = 42_f,
+      .fine_tune = 0.5_f,
+    };
+    
+    PolypticOscillator<kBlockSize> osc{params};
 
-    while(size -= kBlockSize) {
+    f root = 20_f;
+    
+    while (size -= kBlockSize) {
 
-      Frame out[kBlockSize];
-      Block<Frame> output {out, kBlockSize};
+      f noise = (Random::Float01() * 2_f - 1_f);
+      noise *= 0.1_f;
+      root += 0.001_f;
+      params.root = root;// + noise;
 
-      // osc.Process(params, output);
+      Buffer<Frame, kBlockSize> buf;
+      osc.Process(buf);
+      // std::cout << buf[0].l.repr() << ", "
+      //           << buf[0].r.repr()
+      //           << '\n';
+
+      Buffer<std::pair<s1_15, s1_15>, kBlockSize> output;
+      for (int i = 0; i < kBlockSize; i++) {
+        output[i] = {s1_15::wrap(buf[i].l), s1_15::wrap(buf[i].r) };
+      }
 
       // write
-      wav_.Write(out, kBlockSize);
+      wav_.Write(output);
+      // std::cout << size << '\n';
     }
   }
 } _;
