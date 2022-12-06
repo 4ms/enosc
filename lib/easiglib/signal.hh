ba56d6d29d5f42cc9c25c5c6b45f1aaf7c7b562c
Dan Green <danngreen1@gmail.com>
2022-12-06 15:49:49 -0800

    signal: Fix u0_8 crossfade when x > y
    if (y-x)*p is negative, converting it to_unsigned() results in the wrong value.
    
    Use signed addition with that negative value and x, and afterwards convert to unsigned.
    
    Added tests for crossfade

--- lib/easiglib/signal.hh
+++ lib/easiglib/signal.hh
@@ -24,7 +24,7 @@ struct Signal {
   }
 
   static constexpr u0_8 crossfade(u0_8 x, u0_8 y, u0_8 phase) {
-    return x + u0_8::narrow(((y.to_signed() - x.to_signed()) * phase.to_signed()).to_unsigned());
+    return (x.to_signed() + s1_7::narrow((y.to_signed() - x.to_signed()) * phase.to_signed())).to_unsigned();
   }
 
   static constexpr s1_15 crossfade_with_diff(s1_15 a, s1_15 d, u0_32 fractional) {
@@ -54,3 +54,19 @@ struct Signal {
     return ((x - p) / (1_f - 2_f * p)).min(1_f).max(0_f);
   }
 };
+
+
+static_assert(Signal::crossfade(0.3_u0_8, 0.0_u0_8, 0.0_u0_8).repr() == (0.3_u0_8).repr());
+static_assert((0.3_u0_8).repr() == 76);
+
+static_assert(Signal::crossfade(0.3_u0_8, 0.0_u0_8, 0.1_u0_8).repr() == (0.269_u0_8).repr()); 
+static_assert((0.269_u0_8).repr() == 68); //should include 0.3 - (0.1*0.3) = 0.27, but it's close!
+static_assert((0.268_u0_8).repr() == 68); 
+static_assert((0.267_u0_8).repr() == 68); 
+static_assert((0.266_u0_8).repr() == 68); 
+
+static_assert(Signal::crossfade(0.3_u0_8, 0.0_u0_8, 0.5_u0_8).repr() == (0.15_u0_8).repr());
+static_assert((0.15_u0_8).repr() == 38);
+
+static_assert(Signal::crossfade(0.3_u0_8, 0.0_u0_8, 0.9_u0_8).repr() == (0.027_u0_8).repr());
+static_assert((0.027_u0_8).repr() == 6);
